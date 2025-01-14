76e09d2f129baa5bf0d8818a5b64fa9619983b4b
Daniel Sauble <daniel@prefect.io>
2025-01-14 14:26:26 -0800

    Update code to match the current tutorials

--- create_deployment.py
+++ create_deployment.py
@@ -3,7 +3,12 @@ from prefect import flow
 if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/prefecthq/demos.git",
-        entrypoint="my_gh_workflow.py:repo_info",
+        entrypoint="my_workflow.py:show_stars",
+        parameters={"github_repos": [
+            "PrefectHQ/prefect",
+            "pydantic/pydantic",
+            "huggingface/transformers"
+        ]},
     ).deploy(
         name="my-first-deployment",
         work_pool_name="my-managed-pool",
