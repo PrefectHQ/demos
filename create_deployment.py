807f06c79a15e26d2ed8cee929938885bfe19400
Daniel Sauble <daniel@prefect.io>
2025-01-14 14:34:22 -0800

    Move parameters to deploy method

--- create_deployment.py
+++ create_deployment.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/prefecthq/demos.git",
         entrypoint="my_workflow.py:show_stars",
+    ).deploy(
+        name="my-first-deployment",
         parameters={"github_repos": [
             "PrefectHQ/prefect",
             "pydantic/pydantic",
             "huggingface/transformers"
         ]},
-    ).deploy(
-        name="my-first-deployment",
         work_pool_name="my-work-pool",
         cron="0 1 * * *",
     )
