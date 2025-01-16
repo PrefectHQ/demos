acae843c16e80af07daab9b59ccf61ca9d56f117
Daniel Sauble <daniel@prefect.io>
2025-01-16 09:52:58 -0800

    Revert "Merge variables into the module file"
    This reverts commit 2be0fd2d7b574479f789666da9629ddb016d2b18.

--- infra/workspaces/modules/workspace/main.tf
+++ infra/workspaces/modules/workspace/main.tf
@@ -16,26 +16,4 @@ resource "prefect_work_pool" "default" {
   name         = var.work_pool_name
   workspace_id = prefect_workspace.workspace.id
   type         = var.work_pool_type
-}
-
-variable "workspace_name" {
-  type        = string
-  description = "Name of the Prefect workspace"
-}
-
-variable "workspace_handle" {
-  type        = string
-  description = "Handle (slug) for the Prefect workspace"
-}
-
-variable "work_pool_name" {
-  type        = string
-  description = "Name of the default work pool"
-  default     = "my-work-pool"
-}
-
-variable "work_pool_type" {
-  type        = string
-  description = "Type of the work pool"
-  default     = "docker"
-}
+} 
